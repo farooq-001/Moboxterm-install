13b32447aa33eda2befff578bc198342bbf4c602
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-07-06 18:32:36 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+unzip MobaXterm_Portable_v24.2.zip  &&  mv MobaXterm_Portable_v24.2 /home/mobaxterm && sudo apt install wine
+sudo dpkg --add-architecture i386 && sudo apt-get update && sudo  apt-get install wine32:i386
+
+wine /home/mobaxterm/MobaXterm_Personal_24.2.exe
