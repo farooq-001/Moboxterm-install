77672aeaaddd88181acdf2d1b8274dc24d02b3c7
SUNKESULA NELAMPATI BABA FAROOQ <107756656+farooq-001@users.noreply.github.com>
2024-07-06 19:14:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+# https://mobaxterm.mobatek.net/download-home-edition.html
 #!/bin/bash
+wget https://download.mobatek.net/2422024061715901/MobaXterm_Portable_v24.2.zip
 
 unzip MobaXterm_Portable_v24.2.zip  &&  mv MobaXterm_Portable_v24.2 /home/mobaxterm && sudo apt install wine
 sudo dpkg --add-architecture i386 && sudo apt-get update && sudo  apt-get install wine32:i386
 
+rm -rf MobaXterm_Portable_v24.2.zip
 sudo wine /home/mobaxterm/MobaXterm_Personal_24.2.exe
